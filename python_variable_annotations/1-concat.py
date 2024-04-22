74c3d256b5bc4dc53eff35e385cd130a1f54ee09
HB74C21 <6720@holbertonstudents.com>
2024-04-22 11:39:05 +0000

    add for task: 1. Basic annotations - concat

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+#!/bin/usr/env python3
+"""
+The concat function concatenates two strings and returns
+the result as a new string.
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Concatenates two input strings and returns the result as a new string"""
+    return str1 + str2
