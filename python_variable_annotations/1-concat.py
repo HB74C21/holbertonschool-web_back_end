e17212acc2e086a5b372731f48a36597e3728f2c
HB74C21 <6720@holbertonstudents.com>
2024-04-22 11:41:29 +0000

    fix

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,4 +1,4 @@
-#!/bin/usr/env python3
+#!/usr/bin/env python3
 """
 The concat function concatenates two strings and returns
 the result as a new string.
@@ -6,5 +6,5 @@ the result as a new string.
 
 
 def concat(str1: str, str2: str) -> str:
-    """Concatenates two input strings and returns the result as a new string"""
+    """Concatenates two input strings and return the result as a new string"""
     return str1 + str2
