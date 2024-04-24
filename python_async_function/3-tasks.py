91708aef27cd18c9b4fb80fbcacd003af21d9125
HB74C21 <6720@holbertonstudents.com>
2024-04-24 08:17:31 +0000

    Add for task: 3. Tasks

---
+++ python_async_function/3-tasks.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Defines a function to create an asyncio.Task from wait_random function.
+"""
+
+import asyncio
+from typing import Callable
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+def task_wait_random(max_delay: int):
+    """
+    Create an asyncio.Task from wait_random function.
+    """
+    return asyncio.create_task(wait_random(max_delay))
