620b2aa4c4df5aed53e1e225557528145e1b7b5e
HB74C21 <6720@holbertonstudents.com>
2024-04-30 08:14:23 +0000

    fix

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -8,6 +8,6 @@ def schools_by_topic(mongo_collection, topic):
     """
     returns the list of school having a specific topic
     """
-    school_list = mongo_collection.find({"topic": topic})
+    school_list = mongo_collection.find({"topics": topic})
 
     return school_list
