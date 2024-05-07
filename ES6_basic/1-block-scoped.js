40a506b4d30ac14df1f941b2fd88a03d94ce34fb
HB74C21 <6720@holbertonstudents.com>
2024-05-07 09:30:25 +0000

    fix

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,12 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-  /* eslint-disable */
   let task = false;
   let task2 = true;
 
   if (trueOrFalse) {
-    /* eslint-disable */
-    let task = true;
-    let task2 = false;
+    task = true;
+    task2 = false;
   }
 
   return [task, task2];
