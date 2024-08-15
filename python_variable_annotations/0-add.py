e5d08fe491555fbedd549da6c07242ed789e72ef
HB74C21 <6720@holbertonstudents.com>
2024-08-15 12:57:44 +0000

    Add new file for task:  0. Basic annotations - add

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+This function, add, takes two floating-point numbers, a and b
+and returns their sum.
+"""
+
+
+def add(a: float, b: float) -> float:
+    """returns their sum of a + b"""
+    return a + b
